a5805c06ab4ef9886210490e5a978c4b65d9fe59
Renaud Heluin <renaud.heluin@novagaia.fr>
2023-03-21 14:37:14 +0100

    breaking-change, add simpleImage badge

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -1,11 +1,13 @@
+import { graphql } from 'gatsby'
+
 /**
- * Implement Gatsby's Browser APIs in this file.
+ * Implement Gatsby`s Browser APIs in this file.
  *
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/
  */
-exports.onRouteUpdate = (_, pluginOptions) => {
+exports.onRouteUpdate = ({ location }, pluginOptions) => {
   function removeOldData() {
-    const list = ['ecoindex-style', 'ecoindex-badge', 'ecoindex-script']
+    const list = [`ecoindex-style`, `ecoindex-badge`]
     list.forEach(element => {
       const oldElement = document.getElementById(element)
       if (oldElement) {
@@ -19,31 +21,59 @@ exports.onRouteUpdate = (_, pluginOptions) => {
         position: fixed;
         bottom: 10px;
       }`
-    const head = document.getElementsByTagName('head')[0]
-    const css = document.createElement('style')
-    css.id = 'ecoindex-style'
+    const head = document.getElementsByTagName(`head`)[0]
+    const css = document.createElement(`style`)
+    css.id = `ecoindex-style`
     if (css.styleSheet) css.styleSheet.cssText = styles
     else css.appendChild(document.createTextNode(styles))
     head.appendChild(css)
   }
   if (pluginOptions.fixedPosition === true) load_style()
+  function load_simple_badge(location) {
+    console.log(`simple badge`)
+    const canonicalUrl = pluginOptions.siteURL + location.pathname
+    const body = document.getElementsByTagName(`body`)[0]
+    const aElement = document.createElement(`a`)
+    const imgElement = document.createElement(`img`)
+    aElement.setAttribute(
+      `href`,
+      `https://bff.ecoindex.fr/redirect/?url=${canonicalUrl}`
+    )
+    aElement.setAttribute(`target`, `_blank`)
+    aElement.setAttribute(`rel`, `noreferrer`)
+    aElement.id = `ecoindex-badge`
+    imgElement.setAttribute(
+      `src`,
+      `https://bff.ecoindex.fr/badge/?theme=${pluginOptions.theme}&url=${canonicalUrl}`
+    )
+    imgElement.setAttribute(`alt`, `Ecoindex Badge`)
+    imgElement.setAttribute(`width`, `108px`)
+    imgElement.setAttribute(`height`, `32px`)
+    aElement.appendChild(imgElement)
+    body.appendChild(aElement)
+  }
   function load_badge() {
-    const body = document.getElementsByTagName('body')[0]
-    const div = document.createElement('div')
-    div.id = 'ecoindex-badge'
+    console.log(`simple badge`)
+    const body = document.getElementsByTagName(`body`)[0]
+    const div = document.createElement(`div`)
+    div.id = `ecoindex-badge`
     if (pluginOptions.theme) div.setAttribute(`data-theme`, pluginOptions.theme)
     body.appendChild(div)
   }
-  load_badge()
   function load_js() {
-    const head = document.getElementsByTagName('head')[0]
-    const script = document.createElement('script')
-    script.key = 'ecoindex-script'
-    script.id = 'ecoindex-script'
+    const head = document.getElementsByTagName(`head`)[0]
+    const script = document.createElement(`script`)
+    script.key = `ecoindex-script`
+    script.id = `ecoindex-script`
     script.defer = true
-    script.src =
-      'https://cdn.jsdelivr.net/gh/cnumr/ecoindex_badge@2/assets/js/ecoindex-badge.js'
+    script.src = `https://cdn.jsdelivr.net/gh/cnumr/ecoindex_badge@2/assets/js/ecoindex-badge.js`
     head.appendChild(script)
   }
-  load_js()
+  console.log(pluginOptions)
+  if (pluginOptions.useSimpleImage) {
+    load_simple_badge(location)
+  } else {
+    load_badge()
+    load_js()
+  }
 }
